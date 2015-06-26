e6106f7827aa71d4b63653aed9d8514677244e64
kenansubasi <kenansubasiceng@gmail.com>
2015-06-26 16:45:05 +0300

    Update views.py file

--- foodsecrets/views.py
+++ foodsecrets/views.py
@@ -3,12 +3,6 @@ from django.http import HttpResponse
 from django.template import Template, Context, RequestContext, loader
 from django.shortcuts import render
 
+# Ana Sayfa'yı görüntüleyen fonksiyon.
 def index(request):
-   
-
-    return render(request, 'index.html')
-
-def register(request):
-
-
-    return render(request, 'register.html')
+   return render(request, 'index.html')
--- profiles/views.py
+++ profiles/views.py
@@ -10,33 +10,40 @@ from django.contrib.auth import authenticate
 from .models import *
 from .forms import *
 
-def user_add(request):
+# Kullanıcı kayıt işlemini yapan fonksiyon.
+def register(request):
     error = 0
 
     if request.method == 'POST':
         #form = userAdd_form(request.POST)
         form = RegistrationForm(request.POST)
         
+        # Gönderilen formun utgun olup olmadığı kontrol ediliyor.
         if form.is_valid(): 
             data = form.cleaned_data
 
+            # Girilen şifrelerin aynı olup olmadığı kontrol ediliyor.
             if data.get("password") != data.get("password_again"):
                 error = 1
 
+            # Mail adresi daha önceden kaydedilmiş mi kontrol ediliyor.
+            profiles_email = User.objects.filter(email=data.get('email'))
+            if profiles_email:
+                error = 2
+
+            # Girilen username'e sahip kullanıcı var mı kontrol ediliyor.
+            profiles_username = User.objects.filter(username=data.get('username'))
+            if profiles_username:
+                error = 3
+
+            # Hata yoksa kişi kayıt işlemi gerçekleşiyor.
             if error == 0:
-            
-                username = data.get('username')
-                first_name = data.get('first_name')
-                last_name = data.get('last_name')
-                email = data.get('email')
-                password = data.get('password')
-            
                 user = User.objects.create_user(
-                    username = username,
-                    first_name = first_name,
-                    last_name = last_name,
-                    email = email,
-                    password = password
+                    username = data.get('username'),
+                    first_name = data.get('first_name'),
+                    last_name = data.get('last_name'),
+                    email = data.get('email'),
+                    password = data.get('password')
                 )
 
                 user_profile = Profile()
@@ -44,14 +51,19 @@ def user_add(request):
                 user_profile.phone = data.get('phone')
                 user_profile.save()
 
-                return HttpResponseRedirect('/user/add/')
+                return HttpResponseRedirect('/user/add/') # Parametre olarak verilen url'e geçer.
             else:
+                # Uygun olan formda yukarıdaki  hatalardan biri varsa gerekli hata mesajları ile 
+                # form gösteriliyor.
                 return render(request, "register.html",
                             { 'form':form, 'title':u'Kullanıcı Ekleme', 'error':error})
         else:
+            # Form uygun değilse ona göre hata mesajları ile beraber form gösteriliyor.
             return render(request, "register.html",
                             { 'form':form, 'title':u'Kullanıcı Ekleme', 'error':error})
     else:
+        # Herhangi bir 'POST' isteği yoksa boş form gösteriliyor.
+
         #form = userAdd_form()
         form = RegistrationForm()
         return render(request, "register.html",
