544d51f2b17581452a1e0b74367d6cae5bd0567d
kenansubasi <kenansubasiceng@gmail.com>
2015-07-03 15:27:31 +0300

    Add extra attribute

--- foods/admin.py
+++ foods/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import *
+from .forms import *
 
 class MaterialListInline(admin.TabularInline):
     model = MaterialList
-    extra = 3
+    extra=3
 
 class NewFood(admin.ModelAdmin):
     inlines = [MaterialListInline]
