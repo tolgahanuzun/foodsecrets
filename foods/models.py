c0570decd2cd68d710b4e7b4aefdf4cf52a24f5b
kenansubasi <kenansubasiceng@gmail.com>
2015-07-06 12:28:23 +0300

    Food models add addingDate field

--- foods/models.py
+++ foods/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
 
 from profiles.models import * 
 from materials.models import *
@@ -26,6 +27,7 @@ class Food(models.Model):
     user = models.ForeignKey(User, verbose_name=u"Kullanıcı")
     food_kind = models.ForeignKey(FoodKind, verbose_name=u"Tür")
     totalCalories = models.IntegerField(default=0, blank=True, verbose_name=u"Toplam Kalori")
+    addingDate = models.DateTimeField(default=timezone.now, verbose_name=u"Eklenme Tarihi")
 
     class Meta:
         verbose_name = u"Yemek"
