2e1ed21db3d11f074121ea96a713855507a13353
kenansubasi <kenansubasiceng@gmail.com>
2015-07-07 13:47:52 +0300

    Edit models.py

--- foods/models.py
+++ foods/models.py
@@ -9,24 +9,24 @@ from django.utils import timezone
 from profiles.models import * 
 from materials.models import *
 
-class FoodKind(models.Model):
-    name = models.CharField(primary_key=True, max_length=100, verbose_name=u"Adı")
+class MealKind(models.Model):
+    kind = models.CharField(primary_key=True, max_length=100, verbose_name=u"Adı")
 
     class Meta:
         verbose_name = u"Yemek Türü"
         verbose_name_plural = u"Yemek Türleri"
 
-        ordering = ["name"] 
+        ordering = ["kind"] 
 
     def __unicode__(self):
-        return self.name
+        return self.kind
 
-class Food(models.Model):
+class Meal(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name=u"Adı")
     description = models.TextField(max_length=1000, verbose_name=u"Tarif")
     user = models.ForeignKey(User, verbose_name=u"Kullanıcı")
-    food_kind = models.ForeignKey(FoodKind, verbose_name=u"Tür")
-    totalCalories = models.IntegerField(default=0, blank=True, verbose_name=u"Toplam Kalori")
+    meal_kind = models.ForeignKey(MealKind, verbose_name=u"Tür")
+    totalCalories = models.FloatField(default=0, blank=True, verbose_name=u"Toplam Kalori")
     addingDate = models.DateTimeField(auto_now_add=True, verbose_name=u"Eklenme Tarihi")
 
     class Meta:
@@ -38,20 +38,19 @@ class Food(models.Model):
     def __unicode__(self):
         return self.name
 
-
 class MaterialList(models.Model):
-    name = models.ForeignKey(Material, verbose_name=u"ADI")
+    material = models.ForeignKey(Material, verbose_name=u"ADI")
     amount = models.IntegerField(validators=[MinValueValidator(1),
                                       MaxValueValidator(2000)], verbose_name=u"Miktar")
-    food = models.ForeignKey(Food, verbose_name=u"Yemek")
+    meal = models.ForeignKey(Meal, verbose_name=u"Yemek")
 
 
     class Meta:
         verbose_name = u"Malzeme Listesi"
         verbose_name_plural = u"Malzeme Listeleri"
 
-        ordering = ["name"] 
+        ordering = ["meal"] 
 
     def __string__(self):
-        return self.name
+        return self.material
  
