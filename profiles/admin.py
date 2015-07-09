d7911def24694ad210d5386fc0dba4844d6dfffd
kenansubasi <kenansubasiceng@gmail.com>
2015-07-09 17:26:57 +0300

    Edit admin.py

--- profiles/admin.py
+++ profiles/admin.py
@@ -10,8 +10,10 @@ admin.site.unregister(User)
 class ProfileInline(admin.StackedInline):
     model = Profile
 
+
 class NewProfile(admin.ModelAdmin):
     inlines = [ProfileInline]
-
+ 
 admin.site.register(User, NewProfile)
+
 
